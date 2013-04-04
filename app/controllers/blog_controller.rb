0af5a90b4dfe55797afbeae93c0cc1f69dbcb5fa
marcelofossrj <marcelo.foss.rj@gmail.com>
2013-04-03 21:22:50 -0300

    added tag and category filters to blog main page

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,11 @@
 class BlogController < ApplicationController
   def index
-    @blogs = Post.where("is_published = ?", true).order("created_at").reverse_order.paginate(:per_page => 3, :page => params[:page])
+    
+    @q = Post.search(params[:q])
+    #@blogs = Post.where("is_published = ?", true).order("created_at").reverse_order.paginate(:per_page => 3, :page => params[:page])
+    @blogs = @q.result(:distinct => true).order(sort_column + ' ' + sort_direction).paginate(:per_page => 3, :page => params[:page])
+    @q.build_condition
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @blogs }
@@ -21,4 +26,14 @@ class BlogController < ApplicationController
       format.json { render json: @blog }
     end
   end
+  
+  private
+
+    def sort_column
+      Post.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+    end  
 end
